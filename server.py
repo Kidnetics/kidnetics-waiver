dfd42e039d7a56d0e5e6c527bcc1ccf5becc7f99
Akif patel <akifpatel@supernovaapps.com>
2015-03-22 21:16:15 -0400

    Finished bug fix

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, Response
 from flask.ext.httpauth import HTTPDigestAuth
-import os
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'kidnetics'
